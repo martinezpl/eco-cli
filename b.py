d369d053a2326c082a0137fb96e518a30ff6165e
Roy Fishermann <mszlesz@gmail.com>
2021-02-05 02:14:06 +0000

    daily concat, input validation, ux

--- b.py
+++ b.py
@@ -5,7 +5,7 @@ import datetime  as dt
 import sys, os
 
 #TO DO:
-#merging into daily
+#update_daily during reports
 #implement functions
 #command mode
 #setup!!
@@ -14,59 +14,76 @@ class Eco:
 
     def __init__(self):
         tables_path = os.path.abspath(sys.argv[0] + "/../tables")
-        assert os.path.exists(tables_path), "Have you lost your tables folder?" 
+        assert os.path.exists(tables_path), "Have you lost your tables folder? Create a new one." 
         
         self.tables_path = tables_path
         self.income_path = tables_path + "/income.csv"
-        self.spending_path = tables_path + "/spending.csv"
-        self.saving_path = tables_path + "/savings.csv"
-
-        self.income_df = pd.read_csv(self.income_path)
-        self.spending_df = pd.read_csv(self.spending_path)
-        self.saving_df = pd.read_csv(self.saving_path)
+        self.spendings_path = tables_path + "/spendings.csv"
+        self.savings_path = tables_path + "/savings.csv"
+        self.daily_path = tables_path + "/daily.csv"
+        try:
+            self.income_df = pd.read_csv(self.income_path)
+            self.spendings_df = pd.read_csv(self.spendings_path)
+            self.savings_df = pd.read_csv(self.savings_path)
+        except:
+            click.secho("~~~~~~~~ Hi! Let's customize the program to your lifestyle. ~~~~~~~", bold=True, color='blue')
+            self.initialize()
         self.intro()
     
+    def update_daily(self):
+        self.daily_df = pd.concat([self.income_df, self.spendings_df, self.savings_df], 
+                ignore_index=True, levels=['income', 'spendings', 'savings'], 
+                copy=False).groupby('date', as_index=False).sum().reset_index(drop=True).drop('sum', axis=1)
+        self.daily_df['in'] = self.income_df[['date', 'sum']].groupby('date').transform('sum')
+        self.daily_df['out'] = self.spendings_df[['date', 'sum']].groupby('date').transform('sum')
+        self.daily_df['saved'] = self.savings_df[['date', 'sum']].groupby('date').transform('sum')
+        self.daily_df.to_csv(self.daily_path, index=False)
+
     def intro(self):
         while True:
             choice = inquirer.list_input("fun fact: mitch caught a body bout a week ago",
                                         choices=['flow', 'config', 'exit'])
             if choice == 'flow':
-                choice = inquirer.list_input("I made a new",
-                                            choices=['spending', 'saving', 'income', 'back'])
-                if choice == 'spending':
-                    self.spending_df = self.spending_df.append(self.query(self.spending_df.columns), ignore_index=True)
-                    self.spending_df.to_csv(self.spending_path, index=False)
-                elif choice == 'income':
-                    self.income_df = self.income_df.append(self.query(self.income_df.columns), ignore_index=True)
-                    self.income_df.to_csv(self.income_path, index=False)
-                elif choice == 'saving': 
-                    self.saving_df = self.saving_df.append(self.query(self.saving_df.columns), ignore_index=True)
-                    self.saving_df.to_csv(self.saving_path, index=False)
-                elif choice == 'back':
-                    continue
+                while(True):
+                    choice = inquirer.list_input("I made a new",
+                                                choices=['spendings', 'savings', 'income', 'back'])
+                    if choice == 'spendings':
+                        self.spendings_df = self.spendings_df.append(self.query(self.spendings_df.columns), ignore_index=True)
+                        self.spendings_df.to_csv(self.spendings_path, index=False)
+                    elif choice == 'income':
+                        self.income_df = self.income_df.append(self.query(self.income_df.columns), ignore_index=True)
+                        self.income_df.to_csv(self.income_path, index=False)
+                    elif choice == 'savings': 
+                        self.savings_df = self.savings_df.append(self.query(self.savings_df.columns), ignore_index=True)
+                        self.savings_df.to_csv(self.savings_path, index=False)
+                    elif choice == 'back':
+                        break
             elif choice == 'config':
                 self.config()
             elif choice == 'exit':
                 return 0
 
+    def initialize(self):
+        income = inquirer.text(message="First, enter your sources of income divided by comma eg. job, freelance, crime")
+        spendings = inquirer.text(message="Now, enter your spending categories divided by comma eg. food, bills, hobby")
+        savings = inquirer.text(message="Lastly, enter your saving categories divided by comma eg. rent, future, guitar")
+        income_cats = ['date'] + income.replace(' ', '').split(',') + ['sum']
+        spendings_cats = ['date'] + spendings.replace(' ', '').split(',') + ['sum']
+        sav_cats = ['date'] + savings.replace(' ', '').split(',') + ['sum']
+        pd.DataFrame(columns=income_cats).to_csv(self.income_path, index=False)
+        pd.DataFrame(columns=spendings_cats).to_csv(self.spendings_path, index=False)
+        pd.DataFrame(columns=sav_cats).to_csv(self.savings_path, index=False)
+        pd.DataFrame().to_csv(self.daily_path, index=False)
+        click.secho("You're set my friend!", fg='green')
 
     def config(self):
         while(True):
-            choice = inquirer.list_input("", choices=['initialize', 'add source of income', 'add spending category', 'back'])
+            choice = inquirer.list_input("", choices=['initialize', 'add source of income', 'add spendings category', 'back'])
             if choice == 'initialize':
-                income = inquirer.text(message="Enter your sources of income divided by comma eg. 'job, youtube, crime'")
-                spending = inquirer.text(message="Enter your spending categories divided by comma eg. food, rent, hobby'")
-                savings = inquirer.text(message="Enter your savings categories divided by comma eg. rent, future, guitar")
-                income_cats = ['date'] + income.replace(' ', '').split(',') + ['sum']
-                spending_cats = ['date'] + spending.replace(' ', '').split(',') + ['sum']
-                sav_cats = ['date'] + savings.replace(' ', '').split(',') + ['sum']
-                pd.DataFrame(columns=income_cats).to_csv(self.income_path, index=False)
-                pd.DataFrame(columns=spending_cats).to_csv(self.spending_path, index=False)
-                pd.DataFrame(columns=sav_cats).to_csv(self.saving_path, index=False)
-                click.secho("You're set my friend!", fg='green')
+                self.initialize()    
             elif choice == 'add source of income':
                 pass
-            elif choice == 'add spending category':
+            elif choice == 'add spendings category':
                 pass
             elif choice == 'back':
                 return 0
@@ -79,8 +96,11 @@ class Eco:
             if choice == 'back':
                 break    
             else:
-                amount = round(float(inquirer.text(message = 'Amount')), 2)
-                new_row.at[choice] = amount
+                try:
+                    amount = round(float(inquirer.text(message = 'Amount')), 2)
+                    new_row.at[choice] = amount
+                except:
+                    print("Don't be naughty!")
         if new_row.notna().any():
             new_row.at['sum'] = new_row.sum()
             new_row.at['date'] = dt.date.today().strftime("%d/%m/%y")
@@ -95,13 +115,13 @@ class Eco:
         daily.append(data, ignore_index=True).to_csv(daily_path, index = False)
 
     def save(r, f):
-        df = pd.read_csv(saving_path)
+        df = pd.read_csv(savings_path)
         data = pd.Series([dt.date.today().strftime("%d/%m/%y"), r, f])
-        df.append(data, ignore_index=True).to_csv(saving_path, index = False)
+        df.append(data, ignore_index=True).to_csv(savings_path, index = False)
 
     def report():
         df = pd.read_csv(daily_path)
-        sav = pd.read_csv(saving_path)
+        sav = pd.read_csv(savings_path)
         last_week_dates = [(dt.date.today() - dt.timedelta(days=x)).strftime("%d/%m/%y") for x in list(range(0, 7))]
         mask = df.date.apply(lambda x: any(date for date in last_week_dates if date in x))
         week = df[mask]
