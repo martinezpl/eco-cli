4f4e3883a47089f3b3fd887d97369a1d9896e04a
Roy Fishermann <mszlesz@gmail.com>
2021-02-17 23:22:06 +0000

    scrappy scrappy

---
+++ eco/eco_scrapper.py
@@ -0,0 +1,10 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+def scrap_fun_fact():
+    url = 'https://www.beagreatteacher.com/daily-fun-fact/'
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, 'html.parser')
+    fun_fact = soup.find('main', class_='content').get_text().split('\n')[6]
+    return fun_fact
